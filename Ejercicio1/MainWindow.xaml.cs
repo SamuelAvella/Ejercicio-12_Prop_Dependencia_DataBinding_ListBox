87aa1830f68954595b956e8b8f700e489c4684fe
Samuel Avella <samu.av.jm18@gmail.com>
2024-12-10 14:37:10 +0100

    first commit

---
+++ Ejercicio1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int TamañoDeTexto
+        {
+            get { return (int)GetValue(TamañoDeTextoProperty); }
+            set { SetValue(TamañoDeTextoProperty, value);}
+        }
+
+        public static readonly DependencyProperty TamañoDeTextoProperty =
+            DependencyProperty.Register("TamañoDeTexto", typeof(int), typeof(MainWindow), new PropertyMetadata(12));
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        { 
+            TamañoDeTexto += 2;
+        }
+    }
+}
---
+++ Ejercicio2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Brush ColorDeFondoTexto
+        {
+            get {return (Brush) GetValue(ColorDeFondoTextoProperty); }
+            set {SetValue(ColorDeFondoTextoProperty, value);}
+        }
+
+        public static readonly DependencyProperty ColorDeFondoTextoProperty =
+            DependencyProperty.Register("ColorDeFondoTexto", typeof(Brush), typeof(MainWindow), new PropertyMetadata(Brushes.Black));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ColorDeFondoTexto = Brushes.LightBlue;
+        }
+    }
+}
